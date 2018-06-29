6dc7bbed7a37fcb863671eb57ba10d2c8166921e
Hiram Cruz <hiram0101@gmail.com>
2018-06-29 12:02:08 -0400

    added actions for fetching and deleting cats/dogs

---
+++ src/actions/cat.js
@@ -0,0 +1,74 @@
+export const FETCH_CAT_REQUEST = 'FETCH_CAT_REQUEST'
+export const fetchCatRequest = () => ({
+  type: FETCH_CAT_REQUEST
+})
+
+export const FETCH_CAT_SUCCESS = 'FETCH_CAT_SUCCESS'
+export const fetchCatSuccess = cat => ({
+  type: FETCH_CAT_SUCCESS,
+  cat
+})
+
+export const FETCH_CAT_ERROR = 'FETCH_CAT_ERROR'
+export const fetchCatError = error => ({
+  type: FETCH_CAT_ERROR,
+  error
+})
+
+export const fetchCat = () => dispatch => {
+  import { apiURL } from '../config'
+  dispatch(fetchCatRequest())
+  return fetch(`${apiURL}/cat`)
+    .then(res => {
+      if (!res.ok) {
+        if (
+          res.headers.has('content-type') &&
+          res.headers.get('content-type').startsWith('application/json')
+        ) {
+          return Promise.reject(res.json())
+        }
+        return Promise.reject({ code: res.status, message: res.statusText })
+      }
+      return res.json()
+    })
+    .then(cat => dispatch(fetchCatSuccess(cat)))
+    .catch(error => dispatch(fetchCatError(error)))
+}
+
+export const DELETE_CAT_REQUEST = 'DELETE_CAT_REQUEST'
+export const deleteCatRequest = () => ({
+  type: DELETE_CAT_REQUEST
+})
+
+export const DELETE_CAT_SUCCESS = 'DELETE_CAT_SUCCESS'
+export const deleteCatSuccess = () => ({
+  type: DELETE_CAT_SUCCESS
+})
+
+export const DELETE_CAT_ERROR = 'DELETE_CAT_ERROR'
+export const deleteCatError = error => ({
+  type: DELETE_CAT_ERROR,
+  error
+})
+
+export const deleteCat = () => dispatch => {
+  import { apiURL } from '../config'
+  dispatch(deleteCatRequest())
+  return fetch(`${apiURL}/cat`, {
+    METHOD: 'DELETE'
+  })
+    .then(res => {
+      if (!res.ok) {
+        if (
+          res.headers.has('content-type') &&
+          res.headers.get('content-type').startsWith('application/json')
+        ) {
+          return Promise.reject(res.json())
+        }
+        return Promise.reject({ code: res.status, message: res.statusText })
+      }
+      return res.json()
+    })
+    .then(cat => dispatch(deleteCatSuccess(cat)))
+    .catch(error => dispatch(deleteCatError(error)))
+}
---
+++ src/actions/dog.js
@@ -0,0 +1,74 @@
+export const FETCH_DOG_REQUEST = 'FETCH_DOG_REQUEST'
+export const fetchdogRequest = () => ({
+  type: FETCH_dog_REQUEST
+})
+
+export const FETCH_DOG_SUCCESS = 'FETCH_DOG_SUCCESS'
+export const fetchDogSuccess = dog => ({
+  type: FETCH_DOG_SUCCESS,
+  dog
+})
+
+export const FETCH_DOG_ERROR = 'FETCH_DOG_ERROR'
+export const fetchDogError = error => ({
+  type: FETCH_DOG_ERROR,
+  error
+})
+
+export const fetchDog = () => dispatch => {
+  import { apiURL } from '../config'
+  dispatch(fetchDogRequest())
+  return fetch(`${apiURL}/dog`)
+    .then(res => {
+      if (!res.ok) {
+        if (
+          res.headers.has('content-type') &&
+          res.headers.get('content-type').startsWith('application/json')
+        ) {
+          return Promise.reject(res.json())
+        }
+        return Promise.reject({ code: res.status, message: res.statusText })
+      }
+      return res.json()
+    })
+    .then(dog => dispatch(fetchDogSuccess(dog)))
+    .dogch(error => dispatch(fetchDogError(error)))
+}
+
+export const DELETE_DOG_REQUEST = 'DELETE_DOG_REQUEST'
+export const deleteDogRequest = () => ({
+  type: DELETE_DOG_REQUEST
+})
+
+export const DELETE_DOG_SUCCESS = 'DELETE_DOG_SUCCESS'
+export const deleteDogSuccess = () => ({
+  type: DELETE_DOG_SUCCESS
+})
+
+export const DELETE_DOG_ERROR = 'DELETE_DOG_ERROR'
+export const deleteDogError = error => ({
+  type: DELETE_DOG_ERROR,
+  error
+})
+
+export const deleteDog = () => dispatch => {
+  import { apiURL } from '../config'
+  dispatch(deleteDogRequest())
+  return fetch(`${apiURL}/dog`, {
+    METHOD: 'DELETE'
+  })
+    .then(res => {
+      if (!res.ok) {
+        if (
+          res.headers.has('content-type') &&
+          res.headers.get('content-type').startsWith('application/json')
+        ) {
+          return Promise.reject(res.json())
+        }
+        return Promise.reject({ code: res.status, message: res.statusText })
+      }
+      return res.json()
+    })
+    .then(cat => dispatch(deleteDogSuccess(cat)))
+    .catch(error => dispatch(deleteDogError(error)))
+}
